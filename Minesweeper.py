e99dfdaab33bfabdb63d49e1678db78c5cfbe422
ljallenii <ljallenii55@gmail.com>
2022-04-14 21:16:28 -0500

    added commands to buttons (changes window size)

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,33 +1,50 @@
-from ctypes import sizeof
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 import random
 
-def setGame(difficulty):
-    x = 0
-    y = 0
-    mines=0
-    # Easy
-    if difficulty == 0:
-        # dimensions
+class Game(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        Frame.grid(self)
+        ttk.Button(self, text="Easy", command=self.easyModeButton).grid(column=1, row=0)
+        ttk.Button(self, text="Medium", command=self.medModeButton).grid(column=1, row=1)
+        ttk.Button(self, text="Hard", command=self.hardModeButton).grid(column=1, row=2)
+
+    def easyModeButton(self):
+        self.master.geometry("400x400")
+
         x = 10
         y = 8
         # Creates a board in terminal of location of bombs
         board = [[None for i in range(x)] for j in range(y)]
         mines = 10
 
-    # Medium
-    elif difficulty == 1:
-        # dimensions
+        board = self.setMines(board, x, y, mines)
+
+        for i in board:
+            print(i)
+
+
+    def medModeButton(self):
+        self.master.geometry("600x600")
+
+         # dimensions
         x = 12
         y = 20
         # Creates a board in terminal of location of bombs
         board = [[None for i in range(x)] for j in range(y)]
         mines = 40
 
-    #Hard
-    else:
+        board = self.setMines(board, x, y, mines)
+
+        for i in board:
+            print(i)
+
+    def hardModeButton(self):
+        self.master.geometry("800x800")
+
         # dimensions
         x = 20
         y = 24
@@ -35,40 +52,39 @@ def setGame(difficulty):
         board = [[None for i in range(x)] for j in range(y)]
         mines = 99
 
-    # Places locations for mines
-    board = setMines(board, x, y, mines)
+        board = self.setMines(board, x, y, mines)
 
-    # Test print
-    for x in board:
-        print(x)
-    
+        for i in board:
+            print(i)
 
-
-def setMines(board, x, y, mines):
-    for i in range(mines):
-        pos = random.randrange(0,x*y-1)
-        posx = int(pos / x)  
-        posy = pos % x
-        while (board[posx][posy] == "*"):
+    def setMines(self, board, x, y, mines):
+        for i in range(mines):
             pos = random.randrange(0,x*y-1)
             posx = int(pos / x)  
             posy = pos % x
-        (board[posx][(posy)]) = "*"
+            while (board[posx][posy] == "*"):
+                pos = random.randrange(0,x*y-1)
+                posx = int(pos / x)  
+                posy = pos % x
+            (board[posx][(posy)]) = "*"
         
-    return board
+        return board
 
-
-setGame(2) # Sets game difficulty
 
 window = Tk()
-frm = ttk.Frame(window, padding=10)
-frm.grid()
+game = Game(window)
 
-ttk.Button(frm, text="Easy").grid(column=1, row=0)
-ttk.Button(frm, text="Medium").grid(column=1, row=1)
-ttk.Button(frm, text="Hard").grid(column=1, row=2)
+window.wm_title("Minesweeper")
 
-ttk.Button(frm, text="Quit", command=window.destroy).grid(column=1, row=4)
+#easyResize = window.geometry("400x400")
+#medResize = window.geometry("600x600")
+
+
+#ttk.Button(frm, text="Easy", command=easyResize).grid(column=1, row=0)
+#ttk.Button(frm, text="Medium", command=medResize).grid(column=1, row=1)
+#ttk.Button(frm, text="Hard").grid(column=1, row=2)
+
+ttk.Button(game, text="Quit", command=window.destroy).grid(column=1, row=4)
 
 
 window.mainloop()
